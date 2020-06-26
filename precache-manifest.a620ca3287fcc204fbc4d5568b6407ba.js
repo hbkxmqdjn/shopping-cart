7e5c401f0b59e53787c4acdca12a46cf20c52da4
Robynn Ding <hbkxmqdjn@gmail.com>
2020-06-26 17:05:18 -0400

    Updates

---
+++ precache-manifest.a620ca3287fcc204fbc4d5568b6407ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9281142905562fba3cb42978375c53e7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "4bdd325c2befd75d6316",
+    "url": "/shopping-cart/static/css/main.c00a73d9.chunk.css"
+  },
+  {
+    "revision": "32c9b4ca7045bc511d99",
+    "url": "/shopping-cart/static/js/2.a6b53d29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.a6b53d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdd325c2befd75d6316",
+    "url": "/shopping-cart/static/js/main.7d82d8ae.chunk.js"
+  },
+  {
+    "revision": "531342bfcad1931b43d0",
+    "url": "/shopping-cart/static/js/runtime-main.d30a4a8b.js"
+  },
+  {
+    "revision": "f96380dc6a33f0153d54bd8678e8deec",
+    "url": "/shopping-cart/static/media/item1.f96380dc.jpg"
+  },
+  {
+    "revision": "f96380dc6a33f0153d54bd8678e8deec",
+    "url": "/shopping-cart/static/media/item2.f96380dc.jpg"
+  },
+  {
+    "revision": "f96380dc6a33f0153d54bd8678e8deec",
+    "url": "/shopping-cart/static/media/item3.f96380dc.jpg"
+  },
+  {
+    "revision": "f96380dc6a33f0153d54bd8678e8deec",
+    "url": "/shopping-cart/static/media/item4.f96380dc.jpg"
+  }
+]);
